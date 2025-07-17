2280a1472a2686f94b27b4ae7cb3e914e1c05a8d
Emile Ohstrom <emo328@caliban.cse.lehigh.edu>
2025-07-16 23:06:34 -0400

    Create arrays for index js and edit pseudocode
    Create asexual and transgender arrays and put them in allFlags array
    Rearrange pseudocode order to work properly by making label arrays first
    Add some extra pseudocode for more details

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 // How do I implement a search that displays only the pride flags with labels that match the search query?
 
+// Create arrays to hold each flag's name, image path, and page path
+const asexual = ["asexual", "./images/sexual-attraction/asexual.svg", "./flag-pages/asexual.html"];
+const transgender = ["transgender", "./images/gender/transgender.svg", "./flag-pages/transgender.html"]
+
 // Create array with an index representing each flag/identity
-// Create arrays inside each array index to hold each flag's name and image path
+const allFlags = [asexual, transgender];
+
 // Create objects for each flag/identity to display to the user
 // If there is no search query, all flag/label objects should be visible
 // Create a selector for the search input
 // Create a function that executes when the search input is changed
 // Iterate through each array member to see which names match the search query
 // If the label name doesn't match, hide it from the display
-// Make sure this loops indefinitely
+// Make sure this loops indefinitely, but only searches the currently displayed flags/labels
