c2c50b9fc3b2a1b57187e36cd0d49f416d4ec553
Emile Ohstrom <emo328@neptune.cse.lehigh.edu>
2025-07-16 20:10:31 -0400

    Add pseudocode for search algorithm

---
+++ script.js
@@ -0,0 +1,11 @@
+// How do I implement a search that displays only the pride flags with labels that match the search query?
+
+// Create array with an index representing each flag/identity
+// Create arrays inside each array index to hold each flag's name and image path
+// Create objects for each flag/identity to display to the user
+// If there is no search query, all flag/label objects should be visible
+// Create a selector for the search input
+// Create a function that executes when the search input is changed
+// Iterate through each array member to see which names match the search query
+// If the label name doesn't match, hide it from the display
+// Make sure this loops indefinitely
