f27be0e5b1421db8eafc631de39229156c8f2a72
Emile Ohstrom <emo328@ceres.cse.lehigh.edu>
2025-07-17 14:53:24 -0400

    Add variables to js file for search function

--- script.js
+++ script.js
@@ -7,9 +7,33 @@ const transgender = ["transgender", "./images/gender/transgender.svg", "./flag-p
 // Create array with an index representing each flag/identity
 const allFlags = [asexual, transgender];
 
+// Create array to be currently shown?
+
 // Create objects for each flag/identity to display to the user
+
 // If there is no search query, all flag/label objects should be visible
+
 // Create a selector for the search input
+let searchQuery = document.getElementById("searchLabels");
+
+// Contains the current search query
+let filteredSearchQuery = searchQuery.value.toUpperCase();
+
+// ul containing the flags and their labels
+let flagList = document.getElementById("search-results");
+
+// li's contained within the ul of flags and labels
+let flagItem = flagList.getElementsByTagName("li");
+
+// Contains the current flag and label li's a element
+let flagLink;
+
+// Contains the p element of the current flag and label's a element
+let flagParagraph;
+
+// Contains the text content of the current label's p element
+let flagName;
+
 // Create a function that executes when the search input is changed
 // Iterate through each array member to see which names match the search query
 // If the label name doesn't match, hide it from the display
